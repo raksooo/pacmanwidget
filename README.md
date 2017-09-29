2d652a194ae99e7ab38e808abd8fab090cf977d0
Oskar Nyberg <oskar@oskarnyberg.com>
2017-09-29 23:38:13 +0200

    Initial commit, moved pacman widget to its own repository

---
+++ README.md
@@ -0,0 +1,23 @@
+# Awesome pacman widget
+A widget for awesome wm which shows the numer of pacman updates as dots in front of a pacman icon.
+<img src="screenshot.png" />
+
+## Download
+```bash
+git clone https://github.com/raksooo/pacmanwidget.git ~/.config/awesome/pacman
+```
+
+## Usage
+```lua
+pacman = require("statusbar.pacman")
+
+local widget = pacman.create(0.7)
+```
+And then add the widget to your wibar.
+
+## Pacman hook
+```bash
+sed -i "s/<user>/$USER/" pacmanWidget.hook
+# cp pacmanWidget.hook /etc/pacman.d/hooks/
+```
+
---
+++ init.lua
@@ -0,0 +1,43 @@
+local gears = require("gears")
+local wibox = require("wibox")
+local awful = require("awful")
+
+local pacman = {}
+
+function pacman.create(dotOpacity)
+  local widget = wibox.widget {
+    {
+      awful.widget.watch("checkupdates", 180, pacman.update),
+      opacity = dotOpacity,
+      layout = wibox.layout.fixed.horizontal
+    },
+    {
+      image = gears.filesystem.get_configuration_dir()
+        .. "statusbar/pacman/pacman_quantum.png",
+      widget = wibox.widget.imagebox
+    },
+    layout = wibox.layout.fixed.horizontal,
+  }
+
+  awful.tooltip({
+    objects = { widget },
+    timer_function = function() return pacman.tooltip end,
+  })
+
+  return widget
+end
+
+function pacman.update(widget, stdout)
+  lines = #split(stdout, "\n")
+  dots = ""
+  for i = 1, math.min(lines, 55) do
+    dots = dots .. ((i-1) % 20 == 0 and "⚫" or "•")
+  end
+  widget:set_markup(dots)
+
+  local tooltip = stdout:gsub("^%s*(.-)%s*$", "%1")
+  pacman.tooltip = "Updates: " .. lines .. "\n\n" .. tooltip
+end
+
+return pacman
+
